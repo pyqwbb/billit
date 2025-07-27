2721c28a9543f35943fdfc06412146a6ba320ffb
Syeong <pyqw_@naver.com>
2025-07-27 14:50:11 +0900

    Feat: 카카오 소셜 로그인 중간 구현

---
+++ src/pages/Member/OAuth/KakaoCallbackPage.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../../../api/axiosInstance';
+
+function KakaoCallbackPage() {
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URL(window.location.href).searchParams;
+    const authorizationCode = urlParams.get('code');
+    if (authorizationCode) {
+      getToken (authorizationCode);
+    }
+  }, []);
+
+  const getToken = async (code) => {
+    try {
+      const response = await api.post(
+        '/api/v1/auth/login/kakao',
+        { authorizationCode: code },
+        { withCredentials: true }
+      );
+
+      const data = response.data;
+      console.log('Kakao login response:', data);
+      if (data.type === 'LOGIN_SUCCESS') {
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate('/');
+      } else if (data.type === 'REGISTER_REQUIRED') {
+        const userInfoKey = data.userInfoKey;
+        navigate('/terms-agreement', { state: { userInfoKey } });
+      }
+    } catch (err) {
+      setError('카카오 로그인 처리 중 오류 발생');
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <p>카카오 로그인 처리 중...</p>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+  );
+}
+
+export default KakaoCallbackPage;
