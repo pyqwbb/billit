2dc4698f1c4d703b952a213d45e137f37fcd913f
Syeong <pyqw_@naver.com>
2025-09-03 10:46:56 +0900

    Fix: 연락처 정보 추가 및 상호명 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -49,10 +49,11 @@ const Footer = () => {
   return (
     <FooterContainer>
       <InfoList>
-        <InfoItem><span>상호</span> 프렌즈</InfoItem>
+        <InfoItem><span>상호</span> 프렌즈(PRIENZ)</InfoItem>
         <InfoItem><span>대표</span> 김우주</InfoItem>
         <InfoItem><span>사업자등록번호</span> 708-71-00730</InfoItem>
         <InfoItem><span>이메일</span> rladnwntjdbs@naver.com</InfoItem>
+        <InfoItem><span>전화번호</span> 010-4141-7301</InfoItem>
         <InfoItem><span>주소</span> 서울특별시 광진구 아차산로 262, B128~131호 건국대학교 캠퍼스타운 사업단 [공유오피스 PRIENZ](자양동,스타시티)</InfoItem>
       </InfoList>
       <DocsList>
